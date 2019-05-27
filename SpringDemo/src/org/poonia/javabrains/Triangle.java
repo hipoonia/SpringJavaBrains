0866efed481c6a4f79b83233c191926c090e0406
hipoonia <prdp295@gmail.com>
2019-05-27 20:09:28 +0530

    #1 way to call constructor by using index

---
+++ SpringDemo/src/org/poonia/javabrains/Triangle.java
@@ -0,0 +1,36 @@
+package org.poonia.javabrains;
+
+public class Triangle {
+	
+	private String type;
+	private int height;
+	
+	public String getType() {
+		return type;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	
+	public Triangle(int height) {
+		this.height = height;
+	}
+	/*
+	 * public void setType(String type) { this.type = type; }
+	 */
+
+	public void draw() {
+		System.out.println(getType()+ " Triangle Drawn of height " + getHeight() );
+	}
+
+}
